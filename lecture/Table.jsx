5366481b509b572d12109d5f3ecd23a5d3bb8546
zeroduck <syd9538@gmail.com>
2022-11-30 21:24:07 +0900

    feat: action 만들어 dispatch 하기, 틱택토 구현하기, 테이블 최적화 하기

--- lecture/Table.jsx
+++ lecture/Table.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import Tr from "./Tr";
 
-const Table = ({onClick, tableData}) => {
+const Table = ({tableData, dispatch}) => {
     return (
-        <table onClick={onClick}>
-            {Array(tableData.length).fill().map((tr, i) => (<Tr rowData={tableData[i]}/>))}
+        <table>
+            {Array(tableData.length).fill().map((tr, i) => (
+                useMemo(() =>
+                        <Tr key={i} dispatch={dispatch} rowIndex={i} rowData={tableData[i]}/>,
+                    [tableData[i]]
+                )
+            ))}
         </table>
     )
 };
--- lecture/Tr.jsx
+++ lecture/Tr.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect, useRef, memo, useMemo} from 'react';
 import Td from "./Td";
 
-const Tr = ({rowData}) => {
+const Tr = memo(({rowData, rowIndex, dispatch}) => {
+    console.log('tr rendered');
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(rowIndex === ref.current[0], dispatch === ref.current[2], rowData === ref.current[3]);
+        ref.current = [rowIndex, dispatch, rowData];
+    }, [rowIndex, dispatch, rowData]);
     return (
         <tr>
-            {Array(rowData.length).fill().map((td) => (<Td>{''}</Td>))}
+            {Array(rowData.length).fill().map((td, i) => (
+            useMemo(() =>
+                <Td key={i} dispatch={dispatch} rowIndex={rowIndex} cellIndex={i} cellData={rowData[i]}>{''}</Td>, [rowData[i]]
+            )
+            ))}
         </tr>
     );
-};
+});
 
 export default Tr;
